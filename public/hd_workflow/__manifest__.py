f9b2054550a2f9cf2122795ce87583af0aff13b7
JinJie <jinj@hxcfsoft.com>
2023-04-14 15:30:25 +0800

    新增工作流文件

---
+++ public/hd_workflow/__manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+{
+    'name': '工作流引擎',
+    'summary': '工作流应用,提供丰富的流程节点管理',
+    'author': 'Jinjie',
+    'website': '',
+    'category': '华东基础/华东基础',
+    'depends' : ['base', 'hr'],
+    'version': '1.0',
+    'data': [
+        # 'security/security_chown.xml',
+        # 'security/security.xml',
+        # 'security/ir.model.access.csv',
+        # 'data/message.xml',
+        # 'data/model_category.xml',
+        # 'data/report_paperformat_data.xml',
+        # 'views/res_users_views.xml',
+        # 'views/todo_all_view.xml',
+        # 'views/ir_process_view.xml',
+        # 'views/amos_role.xml',
+        # 'views/amos_workflow_view.xml',
+        # 'views/amos_personnel_process_record_view.xml',
+        # 'views/basic_attachment_form_view.xml',
+        # 'views/amos_approval_check_user_wizard.xml',
+        # 'views/chown_model.xml',
+        # 'wizard/ban.xml',
+        # 'views/menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    # 'assets': {
+    #         'web.assets_qweb': [
+    #             'amos_workflow/static/src/xml/workflow_buttons.xml',
+    #             'amos_workflow/static/src/xml/todo_menu.xml',
+    #         ],
+    #         'web.assets_backend': [
+    #             'amos_workflow/static/src/js/statebar_invisible.js',
+    #             'amos_workflow/static/src/js/hide_edit_btn.js',
+    #             'amos_workflow/static/src/js/datepicker_widget.js',
+    #             'amos_workflow/static/src/js/radio_do_action.js',
+    #             'amos_workflow/static/src/css/pretty-checkbox.min.css',
+    #             'amos_workflow/static/src/js/todo_menu.js',
+    #             'amos_workflow/static/src/js/todo_all_kanban_record.js',
+    #             'amos_workflow/static/src/css/signature.css'
+    #         ]
+    # },
+    'license': 'LGPL-3',
+}
