bdad7a0925f868366f25cc495a7dbc10eda8d162
JinJie <jinj@hxcfsoft.com>
2023-05-15 14:08:44 +0800

    新增

--- public/hd_workflow/models/__init__.py
+++ public/hd_workflow/models/__init__.py
@@ -2,6 +2,7 @@
 from . import ir_ui_view
 from . import res_users
 from . import ir_attachment
+from . import hd_workflow_mixin
 from . import personnel_process_record
 from . import workflow_role
 from . import ir_process
