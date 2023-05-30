b78a726f833e124e95f0992592d5efccca59afbd
JinJie <jinj@hxcfsoft.com>
2023-05-30 16:42:23 +0800

    新增权重

--- public/hd_workflow/models/res_users.py
+++ public/hd_workflow/models/res_users.py
@@ -4,11 +4,12 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    psignature = fields.Binary(string="个人签名", groups='base.group_user')
+    psignature = fields.Binary(string="个人签名")
+    hd_weight = fields.Integer(string='权重')
 
     @property
     def SELF_WRITEABLE_FIELDS(self):
         """ The list of fields a user can write on their own user record.
         In order to add fields, please override this property on model extensions.
         """
-        return super().SELF_WRITEABLE_FIELDS + ['work_email', 'mobile_phone', 'work_phone', 'psignature']
+        return super().SELF_WRITEABLE_FIELDS + ['work_email', 'mobile_phone', 'work_phone', 'psignature', 'hd_weight']
