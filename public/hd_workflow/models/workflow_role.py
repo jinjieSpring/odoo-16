352e422fc8a808162d9c14497c1f637613095c94
JinJie <jinj@hxcfsoft.com>
2023-06-02 10:42:28 +0800

    新增代码功能

--- public/hd_workflow/models/workflow_role.py
+++ public/hd_workflow/models/workflow_role.py
@@ -12,4 +12,5 @@ class HdWorkflowRole(models.Model):
     name = fields.Char(string='角色名称', required=True)
     code = fields.Char(string='角色代码', required=True)
     users_ids = fields.Many2many('res.users', string='人员')
-    remarks = fields.Text(string='备注', default='无')
+    code_function = fields.Text(string='方法代码')
+    remarks = fields.Text(string='备注', default='人员有数据则优先使用，无数据则使用代码获取人员')
